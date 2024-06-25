cb35a75cd4d8a36862d371d1eb37016c581a7225
Daniel Murphy <murphyda@protonmail.com>
2024-06-25 22:34:34 +0100

    Initial structure of the order book

--- orderbook/orderbook.py
+++ orderbook/orderbook.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from collections import deque
+from sortedcontainers import SortedDict
+from typing import *
 
 class OrderType(Enum):
     BUY = 1
@@ -9,6 +12,18 @@ class OrderType(Enum):
             return "BUY"
         elif self.value == 2:
             return "SELL"
+        else:
+            return ""
+
+
+class PricePoint:
+    def __init__(self):
+        self.limit_price: Optional[int] = None
+        self.size: int = 0
+        self.total_volume: int = 0
+
+        self.head_order: Optional[OrderBookEntry] = None
+        self.tail_order: Optional[OrderBookEntry] = None
 
 
 class OrderBookEntry:
@@ -19,13 +34,47 @@ class OrderBookEntry:
     price, 
     size
     """
-    def __init__(self, id : int, timestamp : float, type : OrderType, price : float, quantity : float):
-        self.id = id
-        self.timestamp = timestamp
-        self.type = type
-        self.price = price
-        self.quantity = quantity
+    def __init__(self, id : int, timestamp : float, type : OrderType, price : int, quantity : int, parent_pricepoint = None):  
+        self.id: int = id
+        self.timestamp: float = timestamp
+        self.type: OrderType = type
+        self.price: int = price
+        self.quantity: int = quantity
+
+        self.next_order = None
+        self.prev_order = None
+        self.parent_pricepoint = parent_pricepoint
     
 
     def __str__(self):
         return f"{self.type} {self.quantity}@{self.price} [{self.id}; {self.timestamp}]" 
+
+
+
+class OrderBookTree:
+    """
+    This would be the buy / sell orders in the order book.
+
+    https://quant.stackexchange.com/questions/3783/what-is-an-efficient-data-structure-to-model-order-book
+
+    https://web.archive.org/web/20110219163448/http://howtohft.wordpress.com/2011/02/15/how-to-build-a-fast-limit-order-book/
+
+    For each price point save a list of orders (in order of arrival).
+    """
+    def __init__(self):
+        self.price_points = SortedDict() # Price: int -> PricePoint
+
+
+
+class OrderBook:
+    def __init__(self):
+        self.history = deque()
+        self.orders: dict[int, OrderBookEntry] = dict() # Maps order id -> order
+
+        self.buy_book = OrderBookTree()
+        self.sell_book = OrderBookTree()
+
+    
+    def process_order(self, order):
+        # Add new order to books
+        ...
--- test/order_test.py
+++ test/order_test.py
@@ -5,6 +5,6 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from orderbook.orderbook import OrderBookEntry, OrderType
 from time import time
 
-test_order = OrderBookEntry(1, time(), "AAPL", OrderType.BUY, 1000, 5)
+test_order = OrderBookEntry(1, time(), OrderType.BUY, 1000, 5)
 
 print(test_order)
